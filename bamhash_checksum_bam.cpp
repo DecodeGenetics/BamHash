fc70630dce0b4cebe505c5da05481a26f8dcc533
Pall Melsted <pmelsted@gmail.com>
2015-05-05 09:27:59 -0700

    Adds the option of including multiple fastq and bam files and running in non-paired-end mode

--- bamhash_checksum_bam.cpp
+++ bamhash_checksum_bam.cpp
@@ -3,10 +3,11 @@
 #include <seqan/bam_io.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 #include <cmath>
 #include <cstdlib>
 #include <stdint.h>
+#include <vector>
 #include <seqan/arg_parse.h>
 
 
@@ -21,14 +22,13 @@ hasFlagSupplementary(seqan::BamAlignmentRecord const & record)
 }
 */
 struct Baminfo {
-  char version[1024];
-  char bindir[1024];
-  seqan::CharString bamFile;
+  std::vector<std::string>  bamfiles;
   bool debug;
   bool noReadNames;
-  bool noQuality; 
+  bool noQuality;
+  bool paired;
 
-  Baminfo() : debug(false), noReadNames(false), noQuality(false) {}
+  Baminfo() : debug(false), noReadNames(false), noQuality(false), paired(true) {}
 
 };
 
@@ -40,9 +40,9 @@ parseCommandLine(Baminfo& options, int argc, char const **argv) {
 
   setShortDescription(parser, "Checksum of a bam file"); //TODO change description
   setVersion(parser, BAMHASH_VERSION);
-  setDate(parser, "Feb 2015");
+  setDate(parser, "May 2015");
 
-  addUsageLine(parser, "[\\fIOPTIONS\\fP] \\fI<in.bam>\\fP");
+  addUsageLine(parser, "[\\fIOPTIONS\\fP] \\fI<in.bam> <in2.bam> ...\\fP");
   addDescription(parser, "Program for checksum of sequence reads. ");
 
   addArgument(parser, seqan::ArgParseArgument(seqan::ArgParseArgument::INPUTFILE,"bamfile", "False", 1));
@@ -55,6 +55,7 @@ parseCommandLine(Baminfo& options, int argc, char const **argv) {
   addOption(parser, seqan::ArgParseOption("d", "debug", "Debug mode. Prints full hex for each read to stdout"));
   addOption(parser, seqan::ArgParseOption("R", "no-readnames", "Do not use read names as part of checksum"));
   addOption(parser, seqan::ArgParseOption("Q", "no-quality", "Do not use read quality as part of checksum"));
+  addOption(parser, seqan::ArgParseOption("P", "no-paired", "Bam files were not generated with paired-end reads"));
 
   // Parse command line.
   seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
@@ -65,7 +66,9 @@ parseCommandLine(Baminfo& options, int argc, char const **argv) {
   options.debug = isSet(parser, "debug");
   options.noReadNames = isSet(parser, "no-readnames");
   options.noQuality = isSet(parser, "no-quality");
-  getArgumentValue(options.bamFile, parser, 0);
+  options.paired = !isSet(parser, "no-paired");
+
+  options.bamfiles = getArgumentValues(parser, 0);
 
   return seqan::ArgumentParser::PARSE_OK;
 }
@@ -80,84 +83,105 @@ int main(int argc, char const **argv) {
     return res == seqan::ArgumentParser::PARSE_ERROR;
   }
 
-  // Open BGZF Stream for reading.
-  seqan::Stream<seqan::Bgzf> inStream;
-  if (!open(inStream, toCString(info.bamFile), "r")) {
-    std::cerr << "ERROR: Could not open " << info.bamFile << " for reading.\n";
-    return 1;
-  }
-
-  // Setup name store, cache, and BAM I/O context.
-  typedef seqan::StringSet<seqan::CharString> TNameStore;
-  typedef seqan::NameStoreCache<TNameStore>   TNameStoreCache;
-  typedef seqan::BamIOContext<TNameStore>     TBamIOContext;
-  TNameStore      nameStore;
-  TNameStoreCache nameStoreCache(nameStore);
-  TBamIOContext   context(nameStore, nameStoreCache);
-
-  // Read header.
-  seqan::BamHeader header;
-  if (readRecord(header, context, inStream, seqan::Bam()) != 0) {
-    std::cerr << "ERROR: Could not read header from BAM file " << info.bamFile << "\n";
-    return 1;
-  }
-  seqan::clear(header);
-
-  // Define:
-  seqan::BamAlignmentRecord record;
   uint64_t sum = 0;
   uint64_t count = 0;
-  seqan::CharString string2hash;
-  //char hexCstr[33];
+  bool pairedWarning = false;
 
-  // Read record
-  while (!atEnd(inStream)) {
-    if (readRecord(record, context, inStream, seqan::Bam()) != 0) {
-      std::cerr << "ERROR: Could not read record from BAM File " << info.bamFile << "\n";
+  
+
+  for (int i = 0; i < info.bamfiles.size(); i++) {
+    // Open BGZF Stream for reading.
+    seqan::Stream<seqan::Bgzf> inStream;
+
+    const char* bamfile = info.bamfiles[i].c_str();
+    
+    if (!open(inStream, bamfile, "r")) {
+      std::cerr << "ERROR: Could not open " << bamfile << " for reading.\n";
       return 1;
     }
-    // Check if flag: reverse complement and change record accordingly
-    if (hasFlagRC(record)) {
-      reverseComplement(record.seq);
-      reverse(record.qual);
+
+    // Setup name store, cache, and BAM I/O context.
+    typedef seqan::StringSet<seqan::CharString> TNameStore;
+    typedef seqan::NameStoreCache<TNameStore>   TNameStoreCache;
+    typedef seqan::BamIOContext<TNameStore>     TBamIOContext;
+    TNameStore      nameStore;
+    TNameStoreCache nameStoreCache(nameStore);
+    TBamIOContext   context(nameStore, nameStoreCache);
+
+    // Read header.
+    seqan::BamHeader header;
+    if (readRecord(header, context, inStream, seqan::Bam()) != 0) {
+      std::cerr << "ERROR: Could not read header from BAM file " << bamfile << "\n";
+      return 1;
     }
-    // Check if flag: supplementary and exclude those
-    if (!hasFlagSupplementary(record) && !hasFlagSecondary(record)) {
-      count +=1;
-      // Construct one string from record
-      if (!info.noReadNames) {
-        seqan::append(string2hash, record.qName);
-        if(hasFlagLast(record)) {
-          seqan::append(string2hash, "/2");
-        } else {
-          seqan::append(string2hash, "/1");
+    seqan::clear(header);
+
+    // Define:
+    seqan::BamAlignmentRecord record;
+    seqan::CharString string2hash;
+    //char hexCstr[33];
+
+    // Read record
+    while (!atEnd(inStream)) {
+      if (readRecord(record, context, inStream, seqan::Bam()) != 0) {
+        std::cerr << "ERROR: Could not read record from BAM File " << bamfile << "\n";
+        return 1;
+      }
+      // Check if flag: reverse complement and change record accordingly
+      if (hasFlagRC(record)) {
+        reverseComplement(record.seq);
+        reverse(record.qual);
+      }
+      // Check if flag: supplementary and exclude those
+      if (!hasFlagSupplementary(record) && !hasFlagSecondary(record)) {
+        count +=1;
+        // Construct one string from record
+        if (!info.noReadNames) {
+          seqan::append(string2hash, record.qName);
+          if(hasFlagLast(record)) {
+            if (info.paired) {
+              seqan::append(string2hash, "/2");
+            } else {
+              if (!pairedWarning) {
+                std::cerr << "WARNING: BamHash was run with --no-paired mode, but BAM file has reads marked as second pair" << std::endl;
+                pairedWarning = true;
+              }
+              seqan::append(string2hash, "/1");
+            }
+          } else {
+            seqan::append(string2hash, "/1");
+          }
         }
-      }
 
-      seqan::append(string2hash, record.seq);
-      if (!info.noQuality) {
-        seqan::append(string2hash, record.qual);
-      }
-      seqan::clear(record);
+        seqan::append(string2hash, record.seq);
+        if (!info.noQuality) {
+          seqan::append(string2hash, record.qual);
+        }
+        seqan::clear(record);
 
-      // Get MD5 hash
-      hash_t hex = str2md5(toCString(string2hash), length(string2hash));
-      seqan::clear(string2hash);
+        // Get MD5 hash
+        hash_t hex = str2md5(toCString(string2hash), length(string2hash));
 
-      if (info.debug) {
-        std::cout << std::hex << hex.p.low << "\n";
-      } else {
-        hexSum(hex, sum);
+
+        if (info.debug) {
+          std::cout << string2hash << " " << std::hex << hex.p.low << "\n";
+        } else {
+          hexSum(hex, sum);
+        }
+
+        seqan::clear(string2hash);
       }
     }
+
+    // print result
+    
   }
 
-  // print result
   if (!info.debug) {
     std::cout << std::hex << sum << "\t";
     std::cout << std::dec << count << "\n";
   }
-
+  
   return 0;
 }
 
--- bamhash_checksum_fastq.cpp
+++ bamhash_checksum_fastq.cpp
@@ -3,23 +3,23 @@
 #include <seqan/seq_io.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 #include <cmath>
 #include <cstdlib>
 #include <stdint.h>
+#include <vector>
 #include <seqan/arg_parse.h>
 
 #include "bamhash_checksum_common.h"
 
 struct Fastqinfo {
-  seqan::CharString fastq1;
-  seqan::CharString fastq2;
+  std::vector<std::string> fastqfiles;
   bool debug;
   bool noReadNames;
   bool noQuality;
   bool paired;
 
-  Fastqinfo() : debug(false), noReadNames(false), noQuality(false) {}
+  Fastqinfo() : debug(false), noReadNames(false), noQuality(false), paired(true) {}
 
 };
 
@@ -31,9 +31,9 @@ parseCommandLine(Fastqinfo& options, int argc, char const **argv) {
 
   setShortDescription(parser, "Checksum of a set of fastq files");
   setVersion(parser, BAMHASH_VERSION);
-  setDate(parser, "Feb 2015");
+  setDate(parser, "May 2015");
 
-  addUsageLine(parser, "[\\fIOPTIONS\\fP] \\fI<in1.fastq.gz>\\fP [\\fIin2.fastq.gz\\fP]");
+  addUsageLine(parser, "[\\fIOPTIONS\\fP] \\fI<in1.fastq.gz>\\fP [\\fIin2.fastq.gz ... \\fP]");
   addDescription(parser, "Program for checksum of sequence reads. ");
 
   addArgument(parser, seqan::ArgParseArgument(seqan::ArgParseArgument::INPUTFILE,"fastqfiles", true));
@@ -45,6 +45,7 @@ parseCommandLine(Fastqinfo& options, int argc, char const **argv) {
   addOption(parser, seqan::ArgParseOption("d", "debug", "Debug mode. Prints full hex for each read to stdout"));
   addOption(parser, seqan::ArgParseOption("R", "no-readnames", "Do not use read names as part of checksum"));
   addOption(parser, seqan::ArgParseOption("Q", "no-quality", "Do not use read quality as part of checksum"));
+  addOption(parser, seqan::ArgParseOption("P", "no-paired", "List of fastq files are not paired-end reads"));
 
   // Parse command line.
   seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
@@ -52,17 +53,14 @@ parseCommandLine(Fastqinfo& options, int argc, char const **argv) {
     return res;
   }
 
-  options.debug = isSet(parser, "debug");
-  options.noReadNames = isSet(parser, "no-readnames");
-  options.noQuality = isSet(parser, "no-quality");
-  getArgumentValue(options.fastq1, parser, 0);
-  if(getArgumentValueCount(parser, 0) > 1) {
-    getArgumentValue(options.fastq2, parser, 0, 1);
-    options.paired = true;
-  } else {
-    options.paired = false;
-  }
+  options.debug = seqan::isSet(parser, "debug");
+  options.noReadNames = seqan::isSet(parser, "no-readnames");
+  options.noQuality = seqan::isSet(parser, "no-quality");
+  options.paired = !seqan::isSet(parser, "no-paired");
 
+  options.fastqfiles = getArgumentValues(parser, 0);
+
+  
   return seqan::ArgumentParser::PARSE_OK;
 }
 
@@ -95,104 +93,124 @@ int main(int argc, char const **argv) {
   seqan::Stream<seqan::GZFile> gzStream1;
   seqan::Stream<seqan::GZFile> gzStream2;
 
-  if (!open(gzStream1, toCString(info.fastq1), "r")) {
-    std::cerr << "ERROR: Could not open the file: " << info.fastq1 << " for reading.\n";
+
+  if (info.paired && (info.fastqfiles.size() % 2 != 0)) {
+    std::cerr << "ERROR: Running with paired end mode, but supplied an odd number of input files ";
+    for (int i = 0; i < info.fastqfiles.size(); i++) {
+      std::cerr << info.fastqfiles[i] << " ";
+    }
+    std::cerr << std::endl;
     return 1;
   }
 
-  if (info.paired && !open(gzStream2, toCString(info.fastq2), "r")) {
-    std::cerr << "ERROR: Could not open the file: " << info.fastq2 << " for reading.\n";
-    return 1;
-  }
-
-  //Setup RecordReader for reading FASTQ file from gzip-compressed file
-  seqan::RecordReader<seqan::Stream<seqan::GZFile>, seqan::SinglePass<> > reader1(gzStream1);
-  seqan::RecordReader<seqan::Stream<seqan::GZFile>, seqan::SinglePass<> > reader2(gzStream2);
-
-  // Read record
-  while (!atEnd(reader1)) {
-    if(info.paired) {
-      if(atEnd(reader2)) { break; }
-    }
-    if (readRecord(id1, seq1, qual1, reader1, seqan::Fastq()) != 0) {
-      if (atEnd(reader1)) {
-        std::cerr << "WARNING: Could not continue reading " << info.fastq1 <<  " at line: " << count+1 << ". Check if files have the same number of reads.\n";
-        return 1;
-      }
-      std::cerr << "ERROR: Could not read from " << info.fastq1 << "\n";
-      return 1;
-    }
-
-    if (info.paired && readRecord(id2, seq2, qual2, reader2, seqan::Fastq()) != 0) {
-      if (atEnd(reader2)) {
-        std::cerr << "WARNING: Could not continue reading " << info.fastq2 << " at line: " << count+1 << ". Check if files have the same number of reads.\n";
-        return 1;
-      }
-      std::cerr << "ERROR: Could not read from " << info.fastq2 << "\n";
-      return 1;
-    }
-
-    count +=1;
-
-
-    // If include id, then cut id on first whitespace
-    if (seqan::endsWith(id1,"/1")) {
-      seqan::strSplit(idSub1, id1, '/', false, 1);
-    } else {
-      seqan::strSplit(idSub1, id1, ' ', false, 1);
-    }
-
+  for (int i = 0; i < info.fastqfiles.size(); i += (info.paired) ? 2 : 1) {
+    const char* fastq1 = info.fastqfiles[i].c_str();
+    const char* fastq2 = "";
     if (info.paired) {
-      if (seqan::endsWith(id2,"/2")) {
-        seqan::strSplit(idSub2, id2, '/', false, 1);
+     fastq2 = info.fastqfiles[i+1].c_str();
+    }
+    
+    if (!open(gzStream1, fastq1, "r")) {
+      std::cerr << "ERROR: Could not open the file: " << fastq1 << " for reading.\n";
+      return 1;
+    }
+
+    if (info.paired && !open(gzStream2, fastq2, "r")) {
+      std::cerr << "ERROR: Could not open the file: " << fastq2 << " for reading.\n";
+      return 1;
+    }
+
+    //Setup RecordReader for reading FASTQ file from gzip-compressed file
+    seqan::RecordReader<seqan::Stream<seqan::GZFile>, seqan::SinglePass<> > reader1(gzStream1);
+    seqan::RecordReader<seqan::Stream<seqan::GZFile>, seqan::SinglePass<> > reader2(gzStream2);
+
+
+    // Read record
+    while (!atEnd(reader1)) {
+      if(info.paired) {
+        if(atEnd(reader2)) { break; }
+      }
+      if (readRecord(id1, seq1, qual1, reader1, seqan::Fastq()) != 0) {
+        if (atEnd(reader1)) {
+          std::cerr << "WARNING: Could not continue reading " << fastq1 <<  " at line: " << count+1 << ".\n";
+          return 1;
+        }
+        std::cerr << "ERROR: Could not read from " << fastq1 << "\n";
+        return 1;
+      }
+
+      if (info.paired && readRecord(id2, seq2, qual2, reader2, seqan::Fastq()) != 0) {
+        if (atEnd(reader2)) {
+          std::cerr << "WARNING: Could not continue reading " << fastq2 << " at line: " << count+1 << ". Check if files have the same number of reads.\n";
+          return 1;
+        }
+        std::cerr << "ERROR: Could not read from " << fastq2 << "\n";
+        return 1;
+      }
+
+      count +=1;
+
+
+      // If include id, then cut id on first whitespace
+      if (seqan::endsWith(id1,"/1") || seqan::endsWith(id1,"/2")) {
+        seqan::strSplit(idSub1, id1, '/', false, 1);
       } else {
-        seqan::strSplit(idSub2, id2, ' ', false, 1);
+        seqan::strSplit(idSub1, id1, ' ', false, 1);
       }
-    }
 
-    // Check if names are in same order in both files
-    if (info.paired && !info.noReadNames && !(idSub1[0] ==  idSub2[0])) {
-      std::cerr << "WARNING: Id_names in line: " << count << " are not in the same order\n";
-      return 1;
-    }
+      if (info.paired) {
+        if (seqan::endsWith(id2,"/1") || seqan::endsWith(id2,"/2")) {
+          seqan::strSplit(idSub2, id2, '/', false, 1);
+        } else {
+          seqan::strSplit(idSub2, id2, ' ', false, 1);
+        }
+      }
 
-    if (!info.noReadNames) {
-      seqan::append(string2hash1, idSub1[0]);
-      seqan::append(string2hash1,"/1");
-    }
-    seqan::append(string2hash1, seq1);
-    if (!info.noQuality) {
-      seqan::append(string2hash1, qual1);
-    }
+      // Check if names are in same order in both files
+      if (info.paired && !info.noReadNames && !(idSub1[0] ==  idSub2[0])) {
+        std::cerr << "WARNING: Id_names in line: " << count << " are not in the same order\n";
+        return 1;
+      }
 
-
-    if (info.paired) {
       if (!info.noReadNames) {
-        seqan::append(string2hash2, idSub2[0]);
-        seqan::append(string2hash2,"/2");
+        seqan::append(string2hash1, idSub1[0]);
+        seqan::append(string2hash1,"/1");
       }
-      seqan::append(string2hash2, seq2);
+      seqan::append(string2hash1, seq1);
       if (!info.noQuality) {
-        seqan::append(string2hash2, qual2);
+        seqan::append(string2hash1, qual1);
       }
+
+
+      if (info.paired) {
+        if (!info.noReadNames) {
+          seqan::append(string2hash2, idSub2[0]);
+          seqan::append(string2hash2,"/2");
+        }
+        seqan::append(string2hash2, seq2);
+        if (!info.noQuality) {
+          seqan::append(string2hash2, qual2);
+        }
+      }
+
+      // Get MD5 hash
+      hex1 = str2md5(toCString(string2hash1), length(string2hash1));
+      if(info.paired) { hex2 = str2md5(toCString(string2hash2), length(string2hash2)); }
+
+      if (info.debug) {
+        std::cout << string2hash1 << " " <<   std::hex << hex1.p.low << "\n";
+        if(info.paired) { std::cout << string2hash2 << std::hex << hex2.p.low << "\n"; }
+      } else {
+        hexSum(hex1, sum);
+        if(info.paired) { hexSum(hex2, sum); }
+      }
+
+      seqan::clear(string2hash1);
+      seqan::clear(string2hash2);
+      seqan::clear(idSub1);
+      seqan::clear(idSub2);
+
     }
-
-    // Get MD5 hash
-    hex1 = str2md5(toCString(string2hash1), length(string2hash1));
-    if(info.paired) { hex2 = str2md5(toCString(string2hash2), length(string2hash2)); }
-
-    if (info.debug) {
-      std::cout << std::hex << hex1.p.low << "\n";
-      if(info.paired) { std::cout << std::hex << hex2.p.low << "\n"; }
-    } else {
-      hexSum(hex1, sum);
-      if(info.paired) { hexSum(hex2, sum); }
-    }
-
-    seqan::clear(string2hash1);
-    seqan::clear(string2hash2);
-    seqan::clear(idSub1);
-    seqan::clear(idSub2);
 
   }
 
@@ -201,6 +219,7 @@ int main(int argc, char const **argv) {
     std::cout << std::dec << count << "\n";
   }
 
+    
   return 0;
 }
 
